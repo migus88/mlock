256c5a3734f8066c826f1b8f5a924bb406d65af8
Yuri Sokolov <7556847+migus88@users.noreply.github.com>
2025-03-25 12:36:07 +0200

    created a setup for canvas example

---
+++ src/mlock-unity-project/Assets/Examples/Car_Canvas/Code/Controls/LockableButton.cs
@@ -0,0 +1,35 @@
+using System;
+using Migs.Examples.Shared;
+using Migs.MLock.Examples.Shared;
+using Migs.MLock.Interfaces;
+using UnityEngine.UI;
+
+namespace Migs.MLock.Examples.Car_Canvas.Code.Controls
+{
+    public class LockableButton : Button, ILockable<CarLockTags>
+    {
+        public CarLockTags LockTags { get; set; }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            CarLockService.Instance.Subscribe(this);
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            CarLockService.Instance.Unsubscribe(this);
+        }
+
+        public void HandleLocking()
+        {
+            interactable = false;
+        }
+
+        public void HandleUnlocking()
+        {
+            interactable = true;
+        }
+    }
+}
