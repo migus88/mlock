9485fd911afae20249b7c6f2aefc39e6be215a17
Yuri Sokolov <7556847+migus88@users.noreply.github.com>
2025-03-25 14:55:42 +0200

    fixed extension methods

--- src/mlock-unity-project/Packages/MLock/Runtime/Debugging/DebugExtensions.cs
+++ src/mlock-unity-project/Packages/MLock/Runtime/Debugging/DebugExtensions.cs
@@ -14,29 +14,35 @@ namespace Migs.MLock.Debugging
         /// </summary>
         /// <param name="service">The lock service to register</param>
         /// <typeparam name="TLockTags">The enum type used for lock tags</typeparam>
+        /// <typeparam name="TCast">Type to cast the lock service into</typeparam>
         /// <returns>The lock service for method chaining</returns>
-        public static ILockService<TLockTags> WithDebug<TLockTags>(this ILockService<TLockTags> service) where TLockTags : Enum
+        public static TCast WithDebug<TLockTags, TCast>(this ILockService<TLockTags> service)
+            where TLockTags : Enum 
+            where TCast : class, ILockService<TLockTags>
         {
-            #if UNITY_EDITOR
+#if UNITY_EDITOR
             DebugData.RegisterLockService(service);
-            #endif
-            
-            return service;
+#endif
+
+            return service as TCast;
         }
-        
+
         /// <summary>
         /// Unregisters this lock service from the debug system
         /// </summary>
         /// <param name="service">The lock service to unregister</param>
         /// <typeparam name="TLockTags">The enum type used for lock tags</typeparam>
+        /// <typeparam name="TCast">Type to cast the lock service into</typeparam>
         /// <returns>The lock service for method chaining</returns>
-        public static ILockService<TLockTags> WithoutDebug<TLockTags>(this ILockService<TLockTags> service) where TLockTags : Enum
+        public static TCast WithoutDebug<TLockTags, TCast>(this ILockService<TLockTags> service)
+            where TLockTags : Enum 
+            where TCast : class, ILockService<TLockTags>
         {
-            #if UNITY_EDITOR
+#if UNITY_EDITOR
             DebugData.UnregisterLockService(service);
-            #endif
-            
-            return service;
+#endif
+
+            return service as TCast;
         }
     }
-} 
+}
